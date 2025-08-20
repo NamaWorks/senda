cbb0e9a49dde205c2d4260054719b3e174d0d4b0
NamaWorks <pedronayamartinez@hotmail.com>
2025-08-20 20:44:04 +0200

    (layout): prepare navbar permanent elements

--- src/components/elements/Nav/Nav.tsx
+++ src/components/elements/Nav/Nav.tsx
@@ -1,16 +1,43 @@
 "use client";
 
+import { ComponentDataType } from "@/utils/types";
 import "./Nav.scss";
 
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { fetchComponent } from "@/utils/actions/serverActions/actions";
+import Button from "@/components/ui/Button/Button";
 
 export default function Nav(){
 
-  const [ toggled, setToggled ] = useState(false);
+  const [ toggled, setToggled ] = useState<boolean>(false);
+  const [ navData, setNavData ] = useState<null | undefined | ComponentDataType>(null)
+
+  useEffect(()=>{
+    const getData = async () => {setNavData(await fetchComponent('nav_bar'))};
+    getData();
+  },[])
 
   return (
     <nav>
-      <p onClick={() => {setToggled(!toggled)}}>{toggled ? "toggled" : "not toggled"}</p>
+      <div className="nav__permanent">
+        <div className="nav__permanent__logo">
+          <Button  
+            fnc={test} 
+            icon={`logo`}
+          />
+        </div>
+        <div className="nav__permanent__btn">
+          <Button 
+            copy={toggled ? "close" : "menu"} 
+            fnc={()=>{setToggled(!toggled)}}
+            // icon={`none`}
+          />
+        </div>
+      </div>
     </nav>
   )
+};
+
+function test ():void {
+  console.log('test');
 };
---
+++ src/components/ui/Button/Button.tsx
@@ -0,0 +1,43 @@
+import "./Button.scss";
+
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+export default function Button({copy, fnc, icon}: {copy?:string, fnc?: React.MouseEventHandler<HTMLButtonElement> | (()=>void) |undefined , icon?:string}) {
+
+
+  const [ iconSrc, setIconSrc ] = useState<string>();
+
+  useEffect(()=>{
+    if(icon==="up_arrow"){setIconSrc('https://moona.dev/senda/wp-content/uploads/2025/08/arrow_icon.svg')};
+    if(icon==="right_arrow"){setIconSrc('https://moona.dev/senda/wp-content/uploads/2025/08/arrow-right_icon.svg')};
+    if(icon==="down_arrow"){setIconSrc('https://moona.dev/senda/wp-content/uploads/2025/08/arrow-down_icon.svg')};
+    if(icon==="left_arrow"){setIconSrc('https://moona.dev/senda/wp-content/uploads/2025/08/arrow-left_icon.svg')};
+    if(icon==="logo"){setIconSrc('https://moona.dev/senda/wp-content/uploads/2025/08/senda_icon_white.svg')};
+    if(icon==="play"){setIconSrc('https://moona.dev/senda/wp-content/uploads/2025/08/play_icon.svg')};
+  },[icon])
+
+  return (
+    <>
+      <button className="button-general" onClick={ fnc }>
+        { icon &&
+          <div className={`btn__icon btn ${icon === "logo" && 'logo-icon'}`}>
+            <Image 
+              fill={true}
+              src={ typeof iconSrc === 'string' ? iconSrc : 'null' } 
+              alt={`button ${icon} icon`} />
+          </div>
+        }
+
+        { copy &&
+          <div className="btn__copy btn">
+            <p className="btn__copy__text">{ copy }</p>
+          </div>
+        }
+
+
+      </button>
+    </>
+  );
+
+};
