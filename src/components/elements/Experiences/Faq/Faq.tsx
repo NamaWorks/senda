103a0c72208feab744ee48704af007af82b03d62
NamaWorks <pedronayamartinez@hotmail.com>
2025-10-03 13:18:45 +0200

    (styles): prepare styles for faq section

--- src/components/elements/Experiences/Faq/Faq.tsx
+++ src/components/elements/Experiences/Faq/Faq.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import "./Faq.scss";
 import { fetchComponent } from "@/utils/actions/serverActions/actions";
 import { ComponentDataType, FaqDataType } from "@/utils/types";
-import { FaqInterface } from "@/utils/interfaces";
+import Image from "next/image";
 
 export default function Faq() {
 
@@ -21,35 +21,29 @@ export default function Faq() {
 
   return (
    <>
-   <div className="faq">
+   <div className="faq" id="faq">
+    <div className="faq__container">
     {
       faqData?.acf?.faq && 
       Object.keys(faqData?.acf.faq as FaqDataType).map((item, i) => {
         return (
-          <div className={`faq-item`} key={`faq-item-${i}`}>
-            <div className="faq-item-question">
-              <p>
-                {
-                  (faqData?.acf.faq as FaqDataType)[item as keyof FaqDataType].question
-                }
-              </p>
+          <div className={`faq__item`} key={`faq-item-${i}`}>
+            <div className="faq__item__question">
+              <p>{(faqData?.acf.faq as FaqDataType)[item as keyof FaqDataType].question}</p>
             </div>
 
-            <div className="faq-item-answer">
-              <p>
-                {
-                  (faqData?.acf.faq as FaqDataType)[item as keyof FaqDataType].answer
-                }
-              </p>
+            <div className="faq__item__answer">
+              <p>{(faqData?.acf.faq as FaqDataType)[item as keyof FaqDataType].answer}</p>
             </div>
 
-            <div className="faq-item-arrow">
-              <img src="https://moona.dev/senda/wp-content/uploads/2025/08/interface-arrows-left-circle-1-arrow-keyboard-circle-button-left.svg" alt="arrow image" />
+            <div className="faq__item__arrow">
+              <Image src="https://moona.dev/senda/wp-content/uploads/2025/08/interface-arrows-left-circle-1-arrow-keyboard-circle-button-left.svg" alt="arrow image" fill={true}/>
             </div>
           </div>
         )
       })
     }
+    </div>
    </div>
    </>
   );
