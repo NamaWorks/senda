3639754cedad43b44e4e6fc12403d608e842bd30
NamaWorks <pedronayamartinez@hotmail.com>
2025-10-22 18:25:35 +0200

    (structure): prepare 404 page // (styles): prepare 404 page styles

--- src/app/global-not-found.tsx
+++ src/app/global-not-found.tsx
@@ -4,27 +4,49 @@ import "./404.scss";
 import "../styles/_all.scss";
 import "./layout.scss";
 import Button from "@/components/ui/Button/Button";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function Custom404() {
+
+  const router = useRouter();
+
   return (
     <>
       <html lang="en">
         <body className={`body`}>
-          <video loop autoPlay muted playsInline width="720">
-            <source src={`https://moona.dev/senda/wp-content/uploads/2025/10/404vid_.mp4`} type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
+          <main className="not-found">
+            <div className="not-found__video__container">
+              <video loop autoPlay muted playsInline width="720">
+                <source
+                  src={`https://moona.dev/senda/wp-content/uploads/2025/10/404vid_.mp4`}
+                  type="video/mp4"
+                />
+                Your browser does not support the video tag.
+              </video>
 
-          <div className="not__found">
-            <div className="not__found__mountains"></div>
-            <div className="not__found__texts">
-              <p>{`you're off route - `}</p>
-              <div>
-                <Button/>
-                <p>this path leads nowhere.</p>
+            </div>
+
+          <div className="not-found__mountains">
+            <Image className='blend-difference' src="https://moona.dev/senda/wp-content/uploads/2025/10/404-dark.svg" alt="top of a mountain illustration" width={1512} height={244}/>
+            <Image  src="https://moona.dev/senda/wp-content/uploads/2025/10/404-white.svg" alt="top of a mountain illustration" width={1512} height={241}/>
+          </div>
+
+          <div className="not-found__copy">
+              <p className="not-found__texts__p">{`You’re off route — `}</p>
+              <div className="not-found__copy__container">
+                <Button
+                  copy={`#404`}
+                  fnc={() => {
+                    router.push('/')
+                  }}
+                  icon="right_arrow"
+                  round={false}
+                />
+                <p className="not-found__texts__p">this path leads nowhere.</p>
               </div>
             </div>
-          </div>
+          </main>
         </body>
       </html>
     </>
